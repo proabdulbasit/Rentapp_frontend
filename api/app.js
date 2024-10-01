edb3487c7e29fdb8e602a29729dd62d1b6edbd2e
Alan Arriaga <alan.arriaga18@gmail.com>
2024-10-01 09:21:02 -0600

    update

--- api/app.js
+++ api/app.js
@@ -65,6 +65,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     return res.sendStatus(200);  // Responder exitosamente a las solicitudes preflight
   }
+  console.log(res)
   next();
 });
 
